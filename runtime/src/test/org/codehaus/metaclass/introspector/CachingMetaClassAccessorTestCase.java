036170310ef2dd0aa8c628bbf4462f02355f921a
pdonald <pdonald@293b64ca-f5d7-0310-b68a-a7ab055f7ba9>
2004-04-18 22:59:56 +0000

    Updated the license tp reflet move out of Spice

--- runtime/src/test/org/codehaus/metaclass/introspector/CachingMetaClassAccessorTestCase.java
+++ runtime/src/test/org/codehaus/metaclass/introspector/CachingMetaClassAccessorTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) The Spice Group. All rights reserved.
+ * Copyright (C) The MetaClass Group. All rights reserved.
  *
  * This software is published under the terms of the Spice
  * Software License version 1.1, a copy of which has been included
